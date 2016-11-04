0fde0d79683e660d7e7cfd2ff6b25e9240bb3970
Gaston Rodriguez <grodriguez@itba.edu.ar>
2016-11-04 16:03:24 -0300

    Agregue la excepcion y newsquarestate tiene builder

---
+++ backEnd/BestPathPossibleException.java
@@ -0,0 +1,16 @@
+package backEnd;
+
+public class BestPathPossibleException extends RuntimeException{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BestPathPossibleException(String Message){ 
+	super(Message); 
+	}
+	
+	public BestPathPossibleException(){ 
+	} 
+}
